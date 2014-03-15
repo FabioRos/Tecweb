edd8af6794670f2de3015af2148e7f4ded36a4fc
StefanoMunari <stefano.munari@hotmail.it>
2014-03-15 02:18:56 +0100

    js esterno

--- public_html/javascript/screen.js
+++ public_html/javascript/screen.js
@@ -1,4 +1,51 @@
-/*jslint browser:true */
+/* SEARCHBAR */
+
 function searchbar(){
-	document.getElementById("text_field").value="";
+ document.getElementById("text_field").value="";
+}
+ function defsearch(){
+ document.getElementById("text_field").value="cerca...";
+}
+
+/* GALLERY */
+
+var index=1,last,last0;
+var li= document.getElementsByClassName("slide");// .length inizia a contare da 1
+
+function next() {
+    document.getElementById("img"+index).setAttribute("class", "hidden");
+    document.getElementById("img0"+index).setAttribute("class","");
+    if( index == li.length)
+        index=1;
+    else
+        index++;
+    last=document.getElementById("img"+index);
+    last0=document.getElementById("img0"+index);
+    last.setAttribute("class", "active");
+    last0.setAttribute("class","activenav");
+    return true;
+}
+
+function previous() {
+ document.getElementById("img"+index).setAttribute("class", "hidden");
+ document.getElementById("img0"+index).setAttribute("class","");
+    if(index == 1)
+        index= li.length;
+    else
+        index--;
+   last=document.getElementById("img"+index);
+   last0=document.getElementById("img0"+index);
+   last.setAttribute("class", "active");
+   last0.setAttribute("class","activenav");
+    return true;
+}
+function change(i){
+    document.getElementById("img"+index).setAttribute("class","hidden");
+    document.getElementById("img0"+index).setAttribute("class","");
+    index= i;
+    last=document.getElementById("img"+index);
+    last0=document.getElementById("img0"+index);
+    last.setAttribute("class","active");
+    last0.setAttribute("class","activenav");
+    return true;
 }
