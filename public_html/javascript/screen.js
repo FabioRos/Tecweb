d7a3ff509ccea572761cd27c308ab5379c36d815
StefanoMunari <stefano.munari@hotmail.it>
2014-03-22 14:47:49 +0100

    merge

--- public_html/javascript/screen.js
+++ public_html/javascript/screen.js
@@ -12,15 +12,23 @@ function searchbar(){
 var index=1,last,last0;
 var li= document.getElementsByClassName("slide");// .length inizia a contare da 1
 
+/*apertura pagina tramite link*/
+// function popUp(winURL){
+//     window.open(winURL,"popup","width=320,height=480");
+// }
+function displayH(){
+    document.getElementById("hiddenLink").setAttribute("class","active");
+}
+
 function next() {
     document.getElementById("img"+index).setAttribute("class", "hidden");
-    document.getElementById("img0"+index).setAttribute("class","");
+    document.getElementById("t"+index).setAttribute("class","");
     if( index == li.length)
         index=1;
     else
         index++;
     last=document.getElementById("img"+index);
-    last0=document.getElementById("img0"+index);
+    last0=document.getElementById("t"+index);
     last.setAttribute("class", "active");
     last0.setAttribute("class","activenav");
     return true;
@@ -28,23 +36,23 @@ function next() {
 
 function previous() {
  document.getElementById("img"+index).setAttribute("class", "hidden");
- document.getElementById("img0"+index).setAttribute("class","");
+ document.getElementById("t"+index).setAttribute("class","");
     if(index == 1)
         index= li.length;
     else
         index--;
    last=document.getElementById("img"+index);
-   last0=document.getElementById("img0"+index);
+   last0=document.getElementById("t"+index);
    last.setAttribute("class", "active");
    last0.setAttribute("class","activenav");
     return true;
 }
 function change(i){
     document.getElementById("img"+index).setAttribute("class","hidden");
-    document.getElementById("img0"+index).setAttribute("class","");
+    document.getElementById("t"+index).setAttribute("class","");
     index= i;
     last=document.getElementById("img"+index);
-    last0=document.getElementById("img0"+index);
+    last0=document.getElementById("t"+index);
     last.setAttribute("class","active");
     last0.setAttribute("class","activenav");
     return true;
